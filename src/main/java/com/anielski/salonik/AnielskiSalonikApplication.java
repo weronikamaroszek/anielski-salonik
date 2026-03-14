33a999275c8b8aab68d9c96105e93393936788f4
weronikamaroszek <weronikamaroszek@gmail.com>
2026-03-14 17:57:33 +0100

    Initial commit - Anielski Salonik frontend

---
+++ src/main/java/com/anielski/salonik/AnielskiSalonikApplication.java
@@ -0,0 +1,13 @@
+package com.anielski.salonik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnielskiSalonikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnielskiSalonikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anielski/salonik/AnielskiSalonikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anielski.salonik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnielskiSalonikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
